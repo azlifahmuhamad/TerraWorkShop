d070ea24d19258dbe432d1411a5375dc60fcd5c2
Norazlifah Ilyati Muhamad <norazlifah.muhamad@infrontconsulting.com.my>
2019-07-29 12:13:09 +0800

    latest

--- Tutorial1/exercise.tf
+++ Tutorial1/exercise.tf
@@ -216,22 +216,19 @@ storage_uri = join(",", azurerm_storage_account.sa.*.primary_blob_endpoint)
 }
 }
 
-//peering
-
-resource "azurerm_virtual_network_peering" "test1" {
-  name = "peeronetotwo"
-  resource_group_name = azurerm_resource_group.rg-test.name
-  virtual_network_name = azurerm_virtual_network.mainvnet.name
-  remote_virtual_network_id = azurerm_virtual_network.vnet2.id
-  allow_virtual_network_access = "true"
+resource "azurerm_virtual_network_peering" "mainvnet" {
+name = "peeronetotwo"
+resource_group_name = azurerm_resource_group.rg-test.name
+virtual_network_name = azurerm_virtual_network.mainvnet.name
+remote_virtual_network_id = azurerm_virtual_network.vnet2.id
+allow_virtual_network_access = "Enabled"
 }
-
-resource "azure_virtual_network_peering" "test2" {
-  name = "peertwotoone"
-  resource_group_name = azurerm_resource_group.rg-test.name
-  virtual_network_name = azurerm_virtual_network.vnet2.name
-  remote_virtual_network_id = azurerm_virtual_network.mainvnet.id
-  allow_virtual_network_access = "true"
+resource "azurerm_virtual_network_peering" "vnet2" {
+name = "peertwotoone"
+resource_group_name = azurerm_resource_group.rg-test.name
+virtual_network_name = azurerm_virtual_network.vnet2.name
+remote_virtual_network_id = azurerm_virtual_network.mainvnet.id
+allow_virtual_network_access = "Enabled"
 }
 
 
